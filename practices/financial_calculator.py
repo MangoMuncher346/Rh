70938f55b592a1c88eb1ad10aa7842d53dc8fffc
MangoMuncher <rohann.hines@ucas-edu.net>
2026-01-26 14:42:42 -0700

    financial_caculator

---
+++ practices/financial_calculator.py
@@ -0,0 +1,21 @@
+# RH Financial Calculator
+
+income = float(input("monthy income: $"))
+
+rent = float(input("rent: $"))
+
+utilities = float(input("utilities: $"))
+
+groceries = float(input("groceries: $"))
+
+transportation = float(input("transportation: $"))
+
+print("monty income: $3000")
+
+print("rent: $1200")
+
+print("utilities: $200")
+
+print("groceries: $250")
+
+print("transportation: $500")
