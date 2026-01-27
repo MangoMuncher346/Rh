1dfbce1c851642cd3b2e5e19d1f74b28c62d8665
MangoMuncher <rohann.hines@ucas-edu.net>
2026-01-27 14:42:10 -0700

    STRINGS

---
+++ notes/strings.py
@@ -0,0 +1,19 @@
+# RH Strings Notes
+
+# name = input("What is your name: ").strip().capitalize()
+
+#print("Hello", name + "!")
+"""
+sentence  = "the quick brown fox jumps over the lazy dog." 
+print(sentence)
+word = input("Tell me a word from the sentence: ")
+new_word = input("Give me a word from the sentence: ")
+start = sentence.find("word")
+length = len("word")
+print(sentence.replace(word, new_word))
+
+#print(sentence[start:start+length])"""
+
+num_one = int(input("Tell me a number: "))
+num_two = int(input("Tell me a number: "))
+print(num_one*num_two)
--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -9,13 +9,3 @@ utilities = float(input("utilities: $"))
 groceries = float(input("groceries: $"))
 
 transportation = float(input("transportation: $"))
-
-print("monty income: $3000")
-
-print("rent: $1200")
-
-print("utilities: $200")
-
-print("groceries: $250")
-
-print("transportation: $500")
