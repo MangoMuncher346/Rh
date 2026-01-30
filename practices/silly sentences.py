45265109e8272e228e98bbf76662fe408026badb
MangoMuncher346 <rohann.hines@ucas-edu.net>
2026-01-30 20:54:05 +0000

    silly

--- practices/silly sentences.py
+++ practices/silly sentences.py
@@ -1,10 +1,13 @@
 # RH Silly Sentences
 
-silly = input("The fluffy toaster: ").capitalize()
+"""silly = input("The fluffy toaster: ").capitalize()
 
 sentenceB = input("danced over" + "?")
 
 sentence = input("the semlly banana: ")
 
-print("Really " + silly + sentence + sentenceB + "? that's great get to it! Im going to go swimming! ")
+print("Really " + silly + sentence + sentenceB + "? that's great get to it! Im going to go swimming! ")"""""
 
+"""adj = input("Whats a adjective?").strip() 
+senetence = "Whats" + adj + "Nice!"
+print(senetence)"""
