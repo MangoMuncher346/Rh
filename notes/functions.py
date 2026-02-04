0c6669b9055a136b53eb891cb1cf9bad2509d72a
MangoMuncher <rohann.hines@ucas-edu.net>
2026-02-04 14:23:11 -0700

    fizzy

---
+++ notes/functions.py
@@ -0,0 +1,21 @@
+# RH functions
+
+def hello():
+    print("Hello world!")
+
+def full_name(first, last):
+    return f"{first} {last}" 
+
+hello()
+print(full_name("Vienna", "LaRose"))
+name = full_name("Ally", "Wrigth")
+print(f"{name} doesn't like {full_name('Israel', 'Monge')}")
+
+
+def factoral(number):
+    total = 1
+    for i in range(1, number +1):
+        total *= i
+    return total
+for num in range(1, 10):
+    print(f"The factoral of {num} is {factoral(num)}")
--- practices/fizzy_buzz.py
+++ practices/fizzy_buzz.py
@@ -1,12 +1,13 @@
 # RH fizz_buzz
 
-start = 1
-while start <= 50:
-    print (start)
+start = 0
+while start < 50:
     start += 1
-    if start%5 == 0:
-        print("Buzz")
-    elif start%3 == 0:
+    if start % 5 ==0 and start % 3 == 0:
+        print("FizzBuzz") 
+    elif start % 3 == 0:
         print("fizz")
-    elif start%5 ==0 and start%3 == 0:
-        print("FizzBuzz")  
+    elif  start%5 == 0:
+        print("Buzz")
+    else:
+        print(start)
