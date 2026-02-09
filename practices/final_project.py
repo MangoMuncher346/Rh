80bc60d6d3c51fb547cc28ad6047ae0debb2af63
MangoMuncher <rohann.hines@ucas-edu.net>
2026-02-09 14:41:58 -0700

    the cat ran Co-authored-by: burntchocolatechip <burntchocolatechip@users.noreply.github.com>

--- practices/final_project.py
+++ practices/final_project.py
@@ -1,5 +1,41 @@
 # RH IM aal project hang man
+import random 
+words = ["mississippi", "word", "computer science", "bump", "trump" , "claps" , "hydrophilic"]
+
+while words >= "mississippi":
+    print(words)
+words = ("mississippi, word, computer science, bump, trump, claps, hydrophilic") 
+
+
 
 def get_random_word():
-    return random.choice(WORDS)
+    return random.choice(1,8)
+print(words)
 
+
+
+
+
+
+
+
+
+
+
+print( """
+      ________
+      |        |
+      |        |
+      |
+      |
+      |
+ __________""")
+
+print( """
+      ________
+      |        |
+      |        |
+      |
+      |
+      |
+ __________""")
