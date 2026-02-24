3481e0b3879470ac95c1b0a37c6e3d8a8c0b3ce3
MangoMuncher346 <rohann.hines@ucas-edu.net>
2026-02-24 08:44:18 -0700

    the cra ran

--- C/practices/finacial_calc.c
+++ C/practices/finacial_calc.c
@@ -1,15 +1,19 @@
 // This is a comment
 #include <stdio.h>
-#include <math.h>
+
 
 int main(){
-    float income = ;
-    float rent = ;
-     float utilities = ;
-    float groceries = ; 
-    float transportation = ;
-    scanf(income, rent ,utilities,  groceries, transportation);
-    printf("your rent is 3000", rent, " %.2f\nrent: %.1fincome: %d of income");
+    const float income = 
+     float rent = 
+     float utilities = 
+     float groceries =  
+     float transportation = 
+    
+    
+    
+   
+   
+    printf("your rent is", rent, " %.2f\nrent: %.1f\nincome: %d\n of income");
     
     printf("income: %d\n rent: %d\n utilites: %d\n grpoceries: %d\n transportation: %d\n", rent , utilities, groceries, transportation);
 
