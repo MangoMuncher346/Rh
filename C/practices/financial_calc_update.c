1fe21786ca7ce0b37c1dbf775973d9034e25c46e
MangoMuncher346 <rohann.hines@ucas-edu.net>
2026-03-06 14:41:05 -0700

    oierwg

---
+++ C/practices/financial_calc_update.c
@@ -0,0 +1,64 @@
+// This is a comment
+#include <stdio.h>
+
+
+    
+
+
+int main(){
+  float income, rent, utilities, groceries, transportation, savings, remaining;
+
+  printf("What's your monthy income: $");
+ 
+  scanf("%f", &income);
+ 
+  printf("rent: $");
+
+  scanf("%f", rent);
+ 
+  printf("utilities: $");
+ 
+  scanf("%f", &utilities);
+ 
+  printf("Your groceries: $");
+ 
+  scanf("%f", &groceries);
+ 
+  printf("transportatio: $");
+ 
+  scanf("%f", &transportation);
+
+  savings = income *0.10;
+  
+  remaining = income-(rent+utilities+groceries+transportation+savings);
+
+  printf("\n monthly summary\n");
+   
+    printf("savings(10%%):$ %.2f\n", savings);
+  
+    printf("remaining to apend: $%.2f\n", remaining);
+   
+    printf("\n percent of income\n");
+   
+    printf("rent:%.1f%%\n", (rent/income)*100);
+   
+    printf("utillites: %.1f%%\n", (utilities/income)*100);
+   
+    printf("groceries: %.1f%%\n", (groceries/income)*100);
+    
+    printf("transportation: %.1f%%\n", (transportation/income)*100);
+    
+    printf("savings: %.1f%%\n", (savings/income));
+
+
+
+
+
+
+    
+
+
+
+
+    return 0;
+}
